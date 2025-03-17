2bc245cc3517c61695734495a0f7b97b3df351e7
thndlovu <tndlovu@student.ubc.ca>
2025-03-16 19:00:10 -0700

    [C] Implemented Settings Menu
    - Added input handling for toggling the settings menu using the P key.
    - Integrated settings menu logic within the GameManager.
    - Enabled play and quit button functionality.
    - Prevented player input from being processed while the game is paused.
    - Connected UI elements to appropriate methods.

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -8,11 +8,42 @@ public class GameManager : SingletonMonoBehavior<GameManager>
 
     [SerializeField] private CoinCounterUI coinCounter;
 
+    [SerializeField] private InputManager inputManager;
+
+    [SerializeField] private GameObject settingsMenu;
+
+    private bool isSettingsMenuActive;
+
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
+
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
+        DisableSettingsMenu();
+    }
+
+    private void ToggleSettingsMenu(){
+        if(isSettingsMenuActive) DisableSettingsMenu();
+        else EnableSettingsMenu();
+    }
+
+    private void EnableSettingsMenu(){
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isSettingsMenuActive = true;
+    }
+
+    public void DisableSettingsMenu(){
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isSettingsMenuActive = false;
     }
 
     public void IncreaseScore()
@@ -20,5 +51,13 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         score++;
         scoreText.text = $"Score: {score}";
         coinCounter.UpdateScore(score);
+    }
+
+    public void QuitGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit();
+        #endif
     }
 }
--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,8 +6,13 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new();
     public UnityEvent OnJump = new();
     public UnityEvent OnDash = new();
+
+    public UnityEvent OnSettingsMenu = new();
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.P)){
+            OnSettingsMenu?.Invoke();
+        }
         Vector2 input = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
         {
